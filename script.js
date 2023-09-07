10aba5495e97a2d8430942d45c01945be51024bb
Wellick0000 <kashyapshiven2002@gmail.com>
2023-09-07 20:57:32 +0530

    commit after setting the size

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ document.addEventListener('DOMContentLoaded', () => {
     const titleDisplay = document.getElementById('title-display');
     const contentDisplay = document.getElementById('content-display');
     const imageDisplay = document.getElementById('image-display');
+    const moreContentDisplay = document.getElementById('more-content-display');
     const videoDisplay = document.getElementById('video-display');
   
     blogForm.addEventListener('submit', (e) => {
@@ -11,11 +12,13 @@ document.addEventListener('DOMContentLoaded', () => {
       const title = document.getElementById('title').value;
       const content = document.getElementById('content').value;
       const image = document.getElementById('image').files[0];
+      const morecontent = document.getElementById('morecontent').value;
       const video = document.getElementById('video').files[0];
   
       titleDisplay.textContent = title;
       contentDisplay.textContent = content;
       imageDisplay.src = URL.createObjectURL(image);
+      moreContentDisplay.textContent = morecontent;
       videoDisplay.src = URL.createObjectURL(video);
   
       document.getElementById('submitted-content').style.display = 'block';
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,9 @@ module.exports = {
   content: ["*"],
   theme: {
     extend: {
+      //  backgroundImage:{
+      //   'cats': 'url(path_to_image)'
+      // },
       fontFamily :{
         crotah: ["crotah"],
         
